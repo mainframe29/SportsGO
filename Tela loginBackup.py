e01dbaf01267160a866481c78c2a9440ceda131c
KarinePistili <43211581+KarinePistili@users.noreply.github.com>
2018-10-05 09:00:06 -0300

    Add files via upload

--- Tela loginBackup.py
+++ Tela loginBackup.py
@@ -7,11 +7,28 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.relativelayout import RelativeLayout
+from kivy.lang import Builder
 
 global id_session
-class Principal(App):
+
+class Tela(BoxLayout):
+    Builder.load_file('telalogin_graphics.kv')
+    
+    def recebe(self):
+        self.db = DbCon()
+        recUsuario = self.boxUser.text
+        recSenha = self.boxSen.text
+
+        self.db.c.execute("select count(*),id_usu from usuario where nick = '"+str(recUsuario)+"' and senha = '"+str(recSenha)+"'") #coloca recUsuario na instrucao do banco
+        test = self.db.c.fetchone() #busca uma linha
+
+        
+        if(test['count(*)'] == 1):
+            self.confirma_login()
+            id_session = test['id_usu']
+            print(id_session)
+        else:
+            self.login_errado()
     def confirma_login(self):
         b = BoxLayout(orientation='vertical')
 
@@ -34,48 +51,11 @@ class Principal(App):
         pop=Popup(title='Login',content=b)
         pop.open()
         voltar.bind(on_press=pop.dismiss)
-            
+    
+class Principal(App):
     def build(self):
-        self.db = DbCon()
-        box = BoxLayout(orientation = 'vertical')
-        botao = Button(text = 'Entrar',on_press = self.recebe,font_size=30,size_hint = [.5,.4],pos_hint={'x' : 0.25})
-
-        self.boxUser = TextInput(size_hint=[.5,.4],pos_hint={'x' : .25})
-        self.boxSen = TextInput(password = True,size_hint=[.5,.4],pos_hint={'x' : .25})
-
-        label_vazia = Label(text=' ',size_hint=[.4,.4]) #funciona mais e feio
-        label_vazia2 = Label(text=' ',size_hint=[.4,.4]) #funciona mais e feio
-        label_us = Label(text = 'Digite seu usuario',font_size=30,size_hint=[.4,.4],pos_hint={'x' : .25})
-        label_sen = Label(text ='Digite a sua senha',font_size=30,size_hint=[.4,.4],pos_hint={'x' : .25})
-
-        box.add_widget(label_us)
-        box.add_widget(self.boxUser)
-        box.add_widget(label_sen)
-        box.add_widget(self.boxSen)
-        box.add_widget(label_vazia)
-        box.add_widget(botao)
-        box.add_widget(label_vazia2)
-
-        self.db.get_rows()
-        return box
-
-    def recebe(self,botao):
-        self.db = DbCon()
-        recUsuario = self.boxUser.text
-        recSenha = self.boxSen.text
-
-        self.db.c.execute("select count(*),id_usu from usuario where nome = '"+str(recUsuario)+"' and senha = '"+str(recSenha)+"'") #coloca recUsuario na instrucao do banco
-        test = self.db.c.fetchone() #busca uma linha
-
-        
-        if(test['count(*)'] == 1):
-            self.confirma_login()
-            id_session = test['id_usu']
-            print(id_session)
-        else:
-            self.login_errado()
-        
-        
+        return Tela()
+    
 ##########################conexao com banco de dados
 class DbCon:
     def __init__(self):
@@ -86,15 +66,5 @@ class DbCon:
                                   charset = 'utf8mb4',
                                   cursorclass = pymysql.cursors.DictCursor)
         self.c = self.db.cursor()
-        
-    def get_rows(self):
-        global users
-        sql = "SELECT * FROM usuario"
-        self.c.execute(sql)
-        users = self.c.fetchall()
-        
-        for x in users:
-            print(x)
-        return self.c.fetchall()
     
 Principal().run()
